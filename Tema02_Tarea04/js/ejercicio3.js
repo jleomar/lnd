0b175b7f9aa266131234e06201cde16b64ab9eb7
jleomar <jleonmarichal@gmail.com>
2017-05-02 09:51:03 +0100

    Subida Tema 02 Tarea 04

---
+++ Tema02_Tarea04/js/ejercicio3.js
@@ -0,0 +1,5 @@
+function lanzamiento() { 
+	var aleatorio = Math.floor((Math.random() * 6) + 1);
+	imagen = "../imagenes/ejercicio3/dado" + aleatorio + ".png"; 
+	document.images["fotoDado"].src = imagen; 
+}
